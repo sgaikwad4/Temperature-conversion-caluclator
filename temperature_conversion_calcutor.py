a193a7fd81c845c30115f20023d52041a4dd2eb6
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-02-04 14:27:31 -0800

    Greeting, finding temperature and temperature type
     Greeted the user and ask what temperature type they want to find.

--- temperature_conversion_calcutor.py
+++ temperature_conversion_calcutor.py
@@ -1,6 +1,13 @@
 # TASK: Write a program that converts a temperature from Celsius to Fahrenheit.
-# Greet the user and ask if what tempreture type they want to find
+
+# Greet the user and ask what temperature type they want to find
+print("Hello, and welcome to the temperature conversion calculator")
+temp_type = input("What temperature do you want to find? [F/C]").strip().upper()
 # Prompt the user to enter a temperature 
+temp = int(input(f"Enter the temperature:  "))
+
 # Convert the Celsius temperature to Fahrenheit using the formula
+
 # Convert the Fahrenheit temperature to Celsius using the formula
+
 # Display the converted temperature
