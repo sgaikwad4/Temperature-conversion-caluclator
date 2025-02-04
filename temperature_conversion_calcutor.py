27b824e287d769d8722f598b57691a785f96a6c0
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-02-04 14:15:19 -0800

    Edited pseudocode

--- temperature_conversion_calcutor.py
+++ temperature_conversion_calcutor.py
@@ -1,5 +1,6 @@
 # TASK: Write a program that converts a temperature from Celsius to Fahrenheit.
-
-# Prompt the user to enter a temperature in Celsius
+# Greet the user and ask if what tempreture type they want to find
+# Prompt the user to enter a temperature 
 # Convert the Celsius temperature to Fahrenheit using the formula
+# Convert the Fahrenheit temperature to Celsius using the formula
 # Display the converted temperature
