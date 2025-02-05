1cef41936d5a7b582d735891aed97a85b8e19c36
sgaikwad4 <siddhantg612@deltalearns.ca>
2025-02-05 12:44:15 -0800

    Made calculations
    Made calculations for celsuis to fahrenheit and fahrenheit to celsuis.

--- temperature_conversion_calcutor.py
+++ temperature_conversion_calcutor.py
@@ -4,10 +4,15 @@
 print("Hello, and welcome to the temperature conversion calculator")
 temp_type = input("What temperature do you want to find? [F/C]").strip().upper()
 # Prompt the user to enter a temperature 
-temp = int(input(f"Enter the temperature:  "))
+temp = int(input(f"Enter the temperature: "))
 
 # Convert the Celsius temperature to Fahrenheit using the formula
+if temp_type == "F":
+    converted_temp = temp * (9/5) + 32
 
 # Convert the Fahrenheit temperature to Celsius using the formula
+else:
+    converted_temp = (temp - 32) * (5/9)
 
 # Display the converted temperature
+print(converted_temp,temp_type)
